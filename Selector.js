18c0ae7f0493463a6650ac49a59c4430c89e58fb
8Daniyar <daniyar.ramazanov28@fizmat.kz>
2025-03-19 20:27:59 +0500

    Update Selector.js

--- Selector.js
+++ Selector.js
@@ -1,6 +1,11 @@
 window.TestSelection = function ({ tests, onSelect }) {
   return (
     <div className="container">
+      <p id="welcome">
+        Добро пожаловать! Здесь вы можете проходить психологические тесты и
+        узнавать о возможных проблемах с вашим ментальным здоровьем. Берегите
+        себя!
+      </p>
       <h1>Выберите тест</h1>
       {tests.map((test) => (
         <div key={test.id} className="test-card" onClick={() => onSelect(test)}>
